463bb57ead8f2de622a5e67d7bf8fb4f83d1128d
erkalodon <andhika.ragilkesuma@gmail.com>
2021-01-20 16:09:22 +0700

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,12 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RidingClassController;
 
+// Load controller stable
+use App\Http\Controllers\Stable\StableController;
+
+// Load controller app-owner
+use App\Http\Controllers\AppOwner\DashboardController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -34,11 +40,12 @@ Auth::routes([
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-// PROFILE
-Route::get('/profile', function(){
+// USER PROFILE
+Route::get('/profile', function () {
     return view('user.personal-information');
 })->name('profile.personal-information');
-Route::get('/profile/change_password', function(){
+
+Route::get('/profile/change_password', function () {
     return view('user.change-password');
 })->name('profile.change-password');
 
@@ -52,11 +59,25 @@ Route::get('/package/{package}/payment', [RidingClassController::class, 'payment
 Route::get('/package/{package}/payment-confirmation', [RidingClassController::class, 'paymentConfirmation'])->name('package.payment_confirmation');
 
 // STABLE
+Route::get('/stable/dashboard', [StableController::class, 'index'])->name('stable.index');
+Route::get('/stable/{stable}/edit', [StableController::class, 'index'])->name('stable.index');
 
 // COACH
+Route::get('/stable/coach', [CoachController::class, 'index'])->name('stable.coach.index');
+Route::get('/stable/coach/create', [CoachController::class, 'create'])->name('stable.coach.index');
+Route::get('/stable/coach/{coach}/edit', [CoachController::class, 'edit'])->name('stable.coach.index');
+Route::get('/stable/coach/{coach}/destroy', [CoachController::class, 'destroy'])->name('stable.coach.index');
 
 // HORSE
+Route::get('/stable/horse', [HorseController::class, 'index'])->name('stable.horse.index');
+Route::get('/stable/horse/create', [HorseController::class, 'index'])->name('stable.horse.index');
+Route::get('/stable/horse/{horse}/edit', [HorseController::class, 'index'])->name('stable.horse.index');
+Route::get('/stable/horse/{horse}/destroy', [HorseController::class, 'index'])->name('stable.horse.index');
 
-// STABLE
+// SCHEDULE
+Route::get('/stable/schedule', [HorseController::class, 'index'])->name('stable.schedule.index');
+
+// APP OWNER
+Route::get('/app-owner/dashboard', [DashboardController::class, 'index'])->name('app_owner.index');
 
 // Payment Approval
