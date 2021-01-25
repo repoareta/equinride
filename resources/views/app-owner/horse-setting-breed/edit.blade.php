31fc4541ce81d2679c238bbdb5ecc7bfda995fd3
sizuwanoadi <adi15siswanto@gmail.com>
2021-01-25 10:45:19 +0700

    add horse breed management

--- resources/views/app-owner/horse-setting-breed/edit.blade.php
+++ resources/views/app-owner/horse-setting-breed/edit.blade.php
@@ -1,30 +1,28 @@
 <div class="modal fade" id="modalEdit" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
-            <div class="modal-header justify-content-end">
-                
+            <div class="modal-header">
+                <h5 class="modal-title">
+                    EDIT BREED
+                </h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <i aria-hidden="true" class="ki ki-close"></i>
                 </button>
             </div>
             <div class="modal-body">
-                <h2 class="title-text ">
-                    ADD SEX
-                </h2>
-                <form action="{{ route('owner.horse-sex.update')}}" method="POST" id="formHorseSex">
+                <form action="#" method="POST" id="formHorseBreed">
                     @method('PATCH')
                     @csrf
                     <input type="hidden" name="id" id="idData">
                     <div class="form-group row">
                         <div class="col-md-6">
-                            <label>Horse Sex</label>
-                            <input type="text" class="form-control" id="name" name="name" placeholder="Horse Sex">
+                            <div class="form-group">
+                                <label>Horse Breed</label>
+                                <input type="text" class="form-control" id="name" name="name" placeholder="Enter Horse Breed">
+                            </div>
                         </div>												
                     </div>
-                    <div class="modal-footer">											
-                        <button class="btn btn-secondary">RESET</button>
-                        <button type="submit" class="btn btn-add-new font-weight-bold">SAVE</button>
-                    </div>
+                    <button type="submit" class="btn btn-primary font-weight-bold float-right">UPDATE</button>
                 </form>
             </div>
         </div>
