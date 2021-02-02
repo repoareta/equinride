a198f5257e83054f6644713b7765e68d8d843408
sizuwanoadi <adi15siswanto@gmail.com>
2021-02-02 16:56:08 +0700

    update index coach

--- resources/views/stable/coach/index.blade.php
+++ resources/views/stable/coach/index.blade.php
@@ -42,26 +42,6 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @for ($i = 1; $i < 20; $i++)
-                            <tr>
-                                <td>{{ $i }}</td>
-                                <td>Steven</td>
-                                <td>17-01-2002</td>
-                                <td>19 years</td>
-                                <td>Male</td>
-                                <td>1 Years</td>
-                                <td>Yes</td>
-                                <td nowrap="nowrap">
-                                    <a href="{{ route('stable.coach.edit', $i) }}" class="btn btn-clean btn-icon mr-2" title="Edit details">
-                                        <i class="la la-edit icon-xl"></i>
-                                    </a>
-
-                                    <a href="javascript:;" class="btn btn-clean btn-icon mr-2" title="Delete details" id="deleteCoach" data-id="{{ $i }}">
-                                        <i class="la la-trash icon-lg"></i>
-                                    </a>
-                                </td>
-                            </tr>
-                            @endfor
                         </tbody>
                     </table>
                 </div>
@@ -82,7 +62,29 @@
     $(document).ready( function () {
         var t = $('#dataTable').DataTable({
 			scrollX   : true,
-			processing: true
+            processing: true,
+            ordering: true,
+            serverSide: true,
+            ajax: {
+                url : '{!! url()->current() !!}'
+            },
+            language: {
+                processing: '<i class="fa fa-spinner fa-spin fa-2x fa-fw"></i> <br> Loading...'
+            },
+            columns: [
+                {
+                    "data": 'DT_RowIndex',
+                    orderable: false, 
+                    searchable: false
+                },
+				{data: 'name', name: 'name'},
+				{data: 'birth_date', name: 'birth_date'},
+				{data: 'age', name: 'age'},
+				{data: 'sex', name: 'sex'},
+				{data: 'experience', name: 'experience'},
+				{data: 'certified', name: 'certified'},
+				{data: 'action', name: 'action'},
+			]
 		});
         $("#dataTable_filter").append("<a href='{{ route('stable.coach.create') }}' class='btn btn-primary ml-5'>Add New +</a>");
 
