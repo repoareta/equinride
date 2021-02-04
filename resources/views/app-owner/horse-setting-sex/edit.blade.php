1f522867752a5073a1436dfe6fd172ae63b8555f
sizuwanoadi <adi15siswanto@gmail.com>
2021-02-05 01:36:33 +0700

    fix

--- resources/views/app-owner/horse-setting-sex/edit.blade.php
+++ resources/views/app-owner/horse-setting-sex/edit.blade.php
@@ -17,7 +17,7 @@
             <div class="card-header py-3">
                 <div class="card-title align-items-start flex-column">
                     <h3 class="card-label font-weight-bolder text-dark">Edit Horse Sex</h3>
-                    <span class="text-muted font-weight-bold font-size-sm mt-1">Edit your new horse sex</span>
+                    <span class="text-muted font-weight-bold font-size-sm mt-1">Edit your horse sex</span>
                 </div>
             </div>
             <!--end::Header-->
