0e1e326c65b867929e9eeb5770c7f51d52f2ee61
erkalodon <andhika.ragilkesuma@gmail.com>
2021-02-05 11:50:22 +0700

    fix front end

--- resources/views/layouts-frontend/app.blade.php
+++ resources/views/layouts-frontend/app.blade.php
@@ -38,21 +38,26 @@
 			<!--begin::Login-->
 			<div class="login login-1 login-signin-on d-flex flex-column flex-lg-row flex-column-fluid bg-white" id="kt_login">
 				<!--begin::Aside-->
-				<div class="login-aside d-flex flex-column flex-row-auto" style="background-color: #F2C98A;">
+				<div class="login-aside d-flex flex-column flex-row-auto" 
+				style="
+				background-image: url(assets/media/branchsto/bg-front.png); 
+				background-size: cover;
+				background-repeat: no-repeat;
+				background-position: center center;">
 					<!--begin::Aside Top-->
 					<div class="d-flex flex-column-auto flex-column pt-lg-40 pt-15">
 						<!--begin::Aside header-->
 						<a href="#" class="text-center mb-10">
-							<img src="{{ asset('assets/media/logos/logo-letter-1.png') }}" class="max-h-70px" alt="" />
+							<img src="{{ asset('assets/media/branchsto/logo-branchsto.svg') }}" class="max-h-250px w-250px" alt="" />
 						</a>
 						<!--end::Aside header-->
 						<!--begin::Aside title-->
-						<h3 class="font-weight-bolder text-center font-size-h4 font-size-h1-lg" style="color: #986923;">Amazing EQUINRIDE</h3>
+						<h3 class="font-weight-bolder text-center font-size-h4 font-size-h1-lg mb-20" style="color: #ffa152;">AMAZING EQUINRIDE</h3>
 						<!--end::Aside title-->
 					</div>
 					<!--end::Aside Top-->
 					<!--begin::Aside Bottom-->
-					<div class="aside-img d-flex flex-row-fluid bgi-no-repeat bgi-position-y-bottom bgi-position-x-center" style="background-image: url(assets/media/svg/illustrations/login-visual-1.svg)"></div>
+					{{-- <div class="aside-img d-flex flex-row-fluid bgi-no-repeat bgi-position-y-bottom bgi-position-x-center" style="background-image: url(assets/media/branchsto/bg-front.png);"></div> --}}
 					<!--end::Aside Bottom-->
 				</div>
 				<!--begin::Aside-->
