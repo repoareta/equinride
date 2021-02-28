6c23572fd38aa66404300eb213758f6934f8ff3a
sizuwanoadi <adi15siswanto@gmail.com>
2021-02-28 21:17:23 +0700

    fix image datatables

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -103,7 +103,7 @@ class UserController extends Controller
                     <div class="d-flex align-items-center">
                         <div class="symbol symbol-50 symbol-sm flex-shrink-0">
                             <div class="symbol-label">
-                                <img class="h-75 align-self-end" src="'. asset($item->booking_detail->package->photo) .'" alt="photo">
+                                <img class="h-75 align-self-center" src="'. asset($item->booking_detail->package->photo) .'" alt="photo">
                             </div>
                         </div>
                         <div class="d-flex flex-column ml-3">
